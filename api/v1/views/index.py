ce5856db2821ffc51ee6c23a6171d7720d12d11a
rky-ke <ronaldyegon15@gmail.com>
2024-02-07 16:01:21 +0300

    Added Comment

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+"""
+Create an endpoint that retrieves the number of each objects by type:
+"""
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
